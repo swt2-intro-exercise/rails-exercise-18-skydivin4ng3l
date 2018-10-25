91e588ba07d2dd5e1eb0baca4a3d88be9fdcdd19
Chris Schulz <christopher.schulz@student.hpi.de>
2018-10-25 17:51:42 +0000

    Generated Authors and added Resource Authors

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :authors
   
   
   # Example of regular route:
