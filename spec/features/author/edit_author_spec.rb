d99aaf8add5afc6b59f7021e69a915a176d8a44a
Chris Schulz <christopher.schulz@student.hpi.de>
2018-11-05 22:11:26 +0100

    rewrote tests for #6 fixes them

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -1,25 +1,32 @@
 require 'rails_helper'
 
 describe "Edit author page", type: :feature do
+    before(:each) do
+        @author = FactoryBot.create(:author)
+        visit edit_author_path(@author)
+    end
 
-  it "should have text inputs for an author's first name, last name, and homepage" do
-    visit edit_author_path
-    
-    # these are the standard names given to inputs by the form builder
-    expect(page).to have_field('author[first_name]')
-    expect(page).to have_field('author[last_name]')
-    expect(page).to have_field('author[homepage]')
-  end
+    it "should have text inputs for an author's first name, last name, and homepage" do
+        expect(page).to have_field('author[first_name]')
+        expect(page).to have_field('author[last_name]')
+        expect(page).to have_field('author[homepage]')
+    end
+  
+    it "should have prefilled text inputs for an author's first name, last name, and homepage" do
+        expect(find_field('author[first_name]').value).to eq 'Alan'
+        expect(find_field('author[last_name]').value).to eq 'Turing'
+        expect(find_field('author[homepage]').value).to eq 'http://wikipedia.org/Alan_Turing'
+    end
 
-  it 'should hint the user that the last name is missing ' do
-    visit edit_author_path
-    page.fill_in 'author[first_name]', with: 'Alan'
-    page.fill_in 'author[last_name]', with: ''
-    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
-    find('input[type="submit"]').click
-    # @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+    it 'should hint the user that the last name is missing ' do
+        # visit edit_author_path
+        # page.fill_in 'author[first_name]', with: 'Alan'
+        page.fill_in 'author[last_name]', with: ''
+        # page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+        find('input[type="submit"]').click
+        # @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
 
-    expect(page).to have_text("Last name can't be blank") 
-  end
+        expect(page).to have_text("Last name can't be blank") 
+    end
   
 end
--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -20,6 +20,11 @@ describe "author index page", type: :feature do
         expect(page).to have_link 'Show Details', href: author_path(@author)
 
     end
+    
+    it "should have a link to edit each authors data" do
+        expect(page).to have_link 'Edit', href: edit_author_path(@author)
+
+    end
 
     it 'should have a table listing the full names as well as homepages of autors ' do
         expect(page).to have_table "authors_table"
