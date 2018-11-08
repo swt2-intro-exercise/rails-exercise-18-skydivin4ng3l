fc1274a59924c802406d3d409454b9d18c54c9dd
Chris Schulz <christopher.schulz@student.hpi.de>
2018-11-08 22:48:55 +0100

    changes according to #10

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ApplicationRecord
     validates :first_name, :last_name, :homepage, presence: true,
                                                     length: {minimum:3}
-    
+    has_and_belongs_to_many :papers
 
     def name
         self.first_name + ' ' + self.last_name
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,5 @@ class Paper < ApplicationRecord
     validates :title, :venue, :year, presence: true
     validates :year, format: { with: /[1|2][0-9]{3}/, message: "%{value} must be numeric and 4 digits long"}
     #, length: {minimum:3}
+    has_and_belongs_to_many :authors
 end
