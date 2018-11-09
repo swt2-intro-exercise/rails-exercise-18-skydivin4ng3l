b4ec87d749153a80cc064a665c2b0ac0160eaafb
skydivin4ng3l <christopher.schulz@student.hpi.de>
2018-11-09 13:42:25 +0100

    fix for issue #13

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,6 @@ class Paper < ApplicationRecord
     validates :year, format: { with: /[1|2][0-9]{3}/, message: "%{value} must be numeric and 4 digits long"}
     #, length: {minimum:3}
     has_and_belongs_to_many :authors
+    scope :from_year, -> (year) { where(:year => year)}
+    # Ex:- scope :active, -> {where(:active => true)}
 end
